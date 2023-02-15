818a9d37cda4527cc529fe09b1d0397343b8cd4e
dai <83387146+dai2024@users.noreply.github.com>
2023-02-15 04:10:45 +0000

    Update limit.c
    32.1

--- C/limit.c
+++ C/limit.c
@@ -1,6 +1,14 @@
+// In this example, we've defined an array str with a size of 1, 
+// which is the maximum number of characters that can be stored in memory given the [?] MB limit. 
+// The fgets function is used to read the string from the standard input, 
+// and the strlen function is used to find the length of the string, 
+// which represents the number of characters in it.
+  
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+int custom_strlen(const char *);
 
 int main() {
   char str[1] = {'0', '0'};
@@ -12,4 +20,20 @@ int main() {
   if (str[0] != '\0')
     printf("The number of characters in the string is: %d\n", length1 & length2);
   return 0;
+}
+
+// In this implementation, we define a function custom_strlen that takes a pointer to a string as an argument. 
+// The function uses a loop to iterate through the string until it reaches the null terminator '\0'. 
+// The variable count is used to keep track of the number of characters in the string, and is incremented with each iteration of the loop. 
+// Finally, the function returns the value of count, which is the length of the string.
+
+int custom_strlen(const char *str)
+{
+    int count = 0;
+    while (*str != '\0')
+    {
+        count++;
+        str++;
+    }
+    return count;
 }
