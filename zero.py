f276f15c9e8921cd1095cdd992ffb73cc404d0bc
dai <83387146+dai2024@users.noreply.github.com>
2023-02-26 08:10:24 +0000

    Update zero.py
    0 👍

--- zero.py
+++ zero.py
@@ -1,3 +1,27 @@
+"""
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double positive_zero = 0.0;
+    double negative_zero = std::signbit(-0.0) ? -0.0 : 0.0;
+
+    std::cout << "+0 = " << positive_zero << std::endl;
+    std::cout << "-0 = " << negative_zero << std::endl;
+
+    if (positive_zero == negative_zero) {
+        std::cout << "+0 and -0 are equal" << std::endl;
+    } else {
+        std::cout << "+0 and -0 are not equal" << std::endl;
+    }
+
+    return 0;
+}
+//    auto bit = static_cast<int>(positive_zero) ^ static_cast<int>(negative_zero);
+//    std::cout << bit << "\n";
+//    auto bit2 = positive_zero ^ negative_zero;
+//    std::cout << bit2 << "\n";
+"""
 import math
 
 def check_zero(num):
